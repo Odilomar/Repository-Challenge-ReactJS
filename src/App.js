e06f4c73c283e90de78a6d0a29b2f48aff9d5260
Odilomar <odilomar.junior@gmail.com>
2020-06-18 22:19:23 -0400

    :sparkles: Retriving all data from API and showing on Web Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      var { data } = response;
+
+      setRepositories([...data]);
+    });
+  }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -13,18 +25,23 @@ function App() {
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+        <div>
+          <h2>Front-end Repository Challenge with ReactJS</h2>
+          <button onClick={handleAddRepository}>Adicionar</button>
+        </div>
+        <ul data-testid="repository-list">
+          {repositories.map((repository) => {
+            return (
+              <li key={repository.id}>
+                {repository.title}
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
   );
 }
 
