4e6fb5a32ec62865cf0e924e632b74d58436c9f7
Odilomar <odilomar.junior@gmail.com>
2020-06-18 22:20:49 -0400

    :sparkles: Add new data to API and refreshing list of repositories

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./styles.css";
 import api from "./services/api";
 
 function App() {
-
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
@@ -16,7 +15,16 @@ function App() {
   }, []);
 
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      id: "123",
+      url: "https://github.com/josepholiveira",
+      title: `Desafio ReactJS ${Date.now()}`,
+      techs: ["React", "Node.js"],
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
@@ -25,23 +33,23 @@ function App() {
 
   return (
     <div>
-        <div>
-          <h2>Front-end Repository Challenge with ReactJS</h2>
-          <button onClick={handleAddRepository}>Adicionar</button>
-        </div>
-        <ul data-testid="repository-list">
-          {repositories.map((repository) => {
-            return (
-              <li key={repository.id}>
-                {repository.title}
-                <button onClick={() => handleRemoveRepository(repository.id)}>
-                  Remover
-                </button>
-              </li>
-            );
-          })}
-        </ul>
+      <div>
+        <h2>Front-end Repository Challenge with ReactJS</h2>
+        <button onClick={handleAddRepository}>Adicionar</button>
       </div>
+      <ul data-testid="repository-list">
+        {repositories.map((repository) => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
